31ee746359337cc2813b0f0f0580d1afd0fda93d
Daniela Bernal <dbern060@fiu.edu>
2022-09-20 22:20:16 -0400

    Assignment completed

---
+++ Flix1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix1
+//
+//  Created by Daniela Bernal on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
